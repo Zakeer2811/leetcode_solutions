196eab30350bc4521d8829d9da6b9b34ca8542d9
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2024-12-27 18:23:06 +0530

    Time: 203 ms (40.39%), Space: 17.7 MB (10.64%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.py
@@ -1,19 +1,21 @@
 class Solution:
     def countPrimeSetBits(self, left: int, right: int) -> int:
-        def isPrime(n):
-            if n <= 1:  # Fix to handle numbers less than or equal to 1
-                return False
-            isprime = 1
-            for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                    isprime = 0
-                    break
-            return isprime
-
-        ans = 0
-        for i in range(left, right + 1):
-            b = bin(i)[2:]
-            c = b.count('1')
-            if isPrime(c):
-                ans += 1
-        return ans
+            def isPrime(n):
+                if n < 2:  # Numbers less than 2 are not prime
+                    return False
+                if n == 2:  # 2 is the only even prime
+                    return True
+                if n % 2 == 0:  # Eliminate other even numbers
+                    return False
+            # Check divisors from 3 to √n, skipping even numbers
+                for i in range(3, int(n**0.5) + 1, 2):
+                    if n % i == 0:
+                        return False
+                return True
+            ans = 0
+            for i in range(left, right + 1):
+                b = bin(i)[2:]
+                c = b.count('1')
+                if isPrime(c):
+                    ans += 1
+            return ans
