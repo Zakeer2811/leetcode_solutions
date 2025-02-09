4c2653689de336d7342034527ca618e8036b8ec8
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2025-02-09 08:34:11 +0530

    Update 2364-count-number-of-bad-pairs.py

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -8,3 +8,15 @@ class Solution:
             if v>1:
                 res-=v*(v-1)
         return res//2
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        tot = len(nums) * (len(nums) - 1) // 2
+        good = 0
+        dp = {}
+        
+        for i,num in enumerate(nums):
+            v = i - num
+            good += dp.get(v, 0)
+            dp[v] = dp.get(v, 0) + 1
+        
+        return tot - good
