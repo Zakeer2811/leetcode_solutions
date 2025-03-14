2b25ddf102f08e3561282b9356d81150582b554c
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2025-03-14 09:43:07 +0530

    Time: 439 ms (32.46%), Space: 29.6 MB (88.94%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.py
@@ -1,34 +1,17 @@
 class Solution:
-    def maximumCandies(self, candies: List[int], k: int) -> int:
+    def maximumCandies(self, candies: List[int], k: int) -> int:        
+        left, right = 1, max(candies)
+        result = 0
         
-       
-        maxele = max(candies)
-        start = 0
-        end = maxele
-        ans = 0
-
-        while start <= end:
-            mid = start + (end - start) // 2
-
-            if self.fun(candies, mid, k):
-                if mid > ans:
-                    ans = mid
-                start = mid + 1
+        while left <= right:
+            mid = (left + right) // 2
+            
+            children_count = sum(pile // mid for pile in candies)
+            
+            if children_count >= k:
+                result = mid
+                left = mid + 1
             else:
-                end = mid - 1
-
-        return ans
-
-    def fun(self, arr, mid, k):
-        if mid == 0:
-            return True
-
-        count = 0
-        for i in range(len(arr)):
-            temp = arr[i] // mid
-            count += temp
-
-        if count >= k:
-            return True
-
-        return False
+                right = mid - 1
+                
+        return result
