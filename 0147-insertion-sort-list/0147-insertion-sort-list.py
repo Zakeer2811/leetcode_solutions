f9f8da6cadf3a9aba677c8ecf05d276bcc3da894
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2025-06-01 11:51:52 +0530

    Time: 41 ms (65.46%), Space: 18.9 MB (38.49%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.py
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -3,30 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        
-        dummy = ListNode(0)
-        dummy.next = head
-        prev = head
-        curr = head.next
-
+        dummy=ListNode(0,head)
+        prev=head
+        curr=head.next
         while curr:
-            if prev.val <= curr.val:
-                prev = curr
-                curr = curr.next
+            if prev.val<=curr.val:
+                prev=curr
+                curr=curr.next
                 continue
-
-            thead = dummy
-            while thead.next and thead.next.val < curr.val:
-                thead = thead.next
-
-            prev.next = curr.next
-            curr.next = thead.next
-            thead.next = curr
-            curr = prev.next
-
+            thead=dummy
+            while thead.next:
+                if thead.next.val>curr.val:
+                    break
+                thead=thead.next
+            prev.next=curr.next
+            curr.next=thead.next
+            thead.next=curr
+            curr=prev.next
         return dummy.next
