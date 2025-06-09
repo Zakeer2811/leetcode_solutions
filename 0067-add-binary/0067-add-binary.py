0e4a5b677c6ae4c097db73f5a48d75e452790a73
MOHAMMAD JAKIR SHAIK <116374751+Zakeer2811@users.noreply.github.com>
2025-06-09 16:59:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        def b2d(bi, ind, res=0):
+            if ind == len(bi):
+                return res
+            res += int(bi[ind]) * (2 ** (len(bi) - (ind + 1)))
+            return b2d(bi, ind + 1, res)
+
+        def d2b(n):
+            if n == 0:
+                return ''
+            return d2b(n // 2) + str(n % 2)
+        return d2b(b2d(a,0)+b2d(b,0))
+
+        
